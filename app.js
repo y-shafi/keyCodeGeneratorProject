e6a6fb0d0251532c739fa46f372d96baed55ff05
y-shafi <yshafi22@gmail.com>
2021-08-15 03:29:11 +0100

    added reset button

--- app.js
+++ app.js
@@ -12,16 +12,25 @@ window.addEventListener('keypress', (e) => {
 
     <div class ="key">
         ${key}
-        <h1>event.key</h1>
+        <h3>event.key</h3>
     </div>
     <div class ="key">
         ${code}
-        <h1>event.code</h1>
+        <h3>event.code</h3>
     </div>
     <div class ="key">
         ${which}
-        <h1>event.which</h1>
+        <h3>event.which</h3>
         </div>
     `;
+
+});
+
+reset.addEventListener("click", (e) => {
+
+    insert.innerHTML = ` <div class="key">Press Any Key</div>`;
+
+
+
 
 });
